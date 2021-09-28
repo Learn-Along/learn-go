9371f4462e092b8ab607c21451d0a7b7993141ee
Martin Ahindura <martinahindura@gmail.com>
2021-09-28 17:28:19 +0300

    Enhance ordered maps

--- projects/dataframe/types/ordered_map.go
+++ projects/dataframe/types/ordered_map.go
@@ -2,18 +2,33 @@ package types
 
 import "sort"
 
-type orderedMapType map[int]interface{}
+/*
+* Supported types are:
+* - map[int]string
+* - map[int]int
+* - map[int]float64
+* - map[int]bool
+ */
+type OrderedMapType interface{
+	ToSlice() ItemSlice
+	Defragmentize(newOrder []int)
+	Len() int
+}
+// type OrderedMapType map[int]interface{}
+
+type OrderedStringMapType map[int]string
+
+// Gets the length of the OrderedStringMapType
+func (o *OrderedStringMapType) Len() int {
+	return len(*o)
+}
 
 // Converts an ordered map to a slice
-func (o *orderedMapType) ToSlice() []interface{} {
+func (o *OrderedStringMapType) ToSlice() ItemSlice {
 	count := len(*o)
 	indices := make([]int, count)
-	slice := make([]interface{}, count)
+	slice := make([]string, count)
 
-	// FIXME: if the assumption is there are no gaps in this orderedMap,
-	// which assumption is true, as Deframentize needs to be called everytime
-	// there is random update to the map, then concurrency here is possible. try a goroutine and a channel
-	// and two ranges, one, on *o, the other on the channel
 	counter := 0
 	for i := range *o {
 		indices[counter] = i
@@ -31,18 +46,148 @@ func (o *orderedMapType) ToSlice() []interface{} {
 	return slice
 }
 
-// Reorders the orderedMapType ensuring that any gaps in the data are removed
+// Reorders the OrderedMapType ensuring that any gaps in the data are removed
 // So as to go back to a sequantial key list
-func (o *orderedMapType) Defragmentize(newOrder []int) {
-	copyOfO := orderedMapType{}
-	for k, v := range *o {
-		// FIXME: concurrency is possible
-		copyOfO[k] = v
-		delete(*o, k)
-	}	
+func (o *OrderedStringMapType) Defragmentize(newOrder []int) {
+	noOfItems := len(newOrder)
+	cleanedMap := make(OrderedStringMapType, noOfItems)
 
 	for newRow, oldRow := range newOrder {
-		// FIXME: concurrency is possible
-		(*o)[newRow] = copyOfO[oldRow]
+		cleanedMap[newRow] = (*o)[oldRow]
 	}
+
+	for key := range (*o) {
+		delete(*o, key)
+	}
+
+	*o = cleanedMap
+}
+
+type OrderedIntMapType map[int]int
+
+// Gets the length of the OrderedStringMapType
+func (o *OrderedIntMapType) Len() int {
+	return len(*o)
+}
+
+// Converts an ordered map to a slice
+func (o *OrderedIntMapType) ToSlice() ItemSlice {
+	count := len(*o)
+	indices := make([]int, count)
+	slice := make([]int, count)
+
+	counter := 0
+	for i := range *o {
+		indices[counter] = i
+		counter++
+	}
+
+	sort.Slice(indices, func(i, j int) bool {
+		return indices[i] < indices[j]
+	})
+
+	for i, index := range indices {
+		slice[i] = (*o)[index]
+	}
+
+	return slice
+}
+
+// Reorders the OrderedMapType ensuring that any gaps in the data are removed
+// So as to go back to a sequantial key list
+func (o *OrderedIntMapType) Defragmentize(newOrder []int) {
+	noOfItems := len(newOrder)
+	cleanedMap := make(OrderedIntMapType, noOfItems)
+
+	for newRow, oldRow := range newOrder {
+		cleanedMap[newRow] = (*o)[oldRow]
+	}
+
+	*o = cleanedMap
+}
+
+type OrderedFloat64MapType map[int]float64
+
+// Gets the length of the OrderedStringMapType
+func (o *OrderedFloat64MapType) Len() int {
+	return len(*o)
+}
+
+// Converts an ordered map to a slice
+func (o *OrderedFloat64MapType) ToSlice() ItemSlice {
+	count := len(*o)
+	indices := make([]int, count)
+	slice := make([]float64, count)
+
+	counter := 0
+	for i := range *o {
+		indices[counter] = i
+		counter++
+	}
+
+	sort.Slice(indices, func(i, j int) bool {
+		return indices[i] < indices[j]
+	})
+
+	for i, index := range indices {
+		slice[i] = (*o)[index]
+	}
+
+	return slice
+}
+
+// Reorders the OrderedMapType ensuring that any gaps in the data are removed
+// So as to go back to a sequantial key list
+func (o *OrderedFloat64MapType) Defragmentize(newOrder []int) {
+	noOfItems := len(newOrder)
+	cleanedMap := make(OrderedFloat64MapType, noOfItems)
+
+	for newRow, oldRow := range newOrder {
+		cleanedMap[newRow] = (*o)[oldRow]
+	}
+
+	*o = cleanedMap
+}
+
+type OrderedBoolMapType map[int]bool
+
+// Gets the length of the OrderedStringMapType
+func (o *OrderedBoolMapType) Len() int {
+	return len(*o)
+}
+
+// Converts an ordered map to a slice
+func (o *OrderedBoolMapType) ToSlice() ItemSlice {
+	count := len(*o)
+	indices := make([]int, count)
+	slice := make([]bool, count)
+
+	counter := 0
+	for i := range *o {
+		indices[counter] = i
+		counter++
+	}
+
+	sort.Slice(indices, func(i, j int) bool {
+		return indices[i] < indices[j]
+	})
+
+	for i, index := range indices {
+		slice[i] = (*o)[index]
+	}
+
+	return slice
+}
+
+// Reorders the OrderedMapType ensuring that any gaps in the data are removed
+// So as to go back to a sequantial key list
+func (o *OrderedBoolMapType) Defragmentize(newOrder []int) {
+	noOfItems := len(newOrder)
+	cleanedMap := make(OrderedBoolMapType, noOfItems)
+
+	for newRow, oldRow := range newOrder {
+		cleanedMap[newRow] = (*o)[oldRow]
+	}
+
+	*o = cleanedMap
 }
--- projects/dataframe/types/ordered_map_test.go
+++ projects/dataframe/types/ordered_map_test.go
@@ -6,55 +6,115 @@ import (
 	"github.com/learn-along/learn-go/projects/dataframe/utils"
 )
 
-// ToSlice converts an ordered map into a slice, ignoring gaps in indices automatically
-func TestOrderedMap_ToSlice(t *testing.T)  {
+// Len returns the length of the map
+func TestOrderedStringMap_Len(t *testing.T)  {
 	type testRecord struct {
-		input orderedMapType;
-		expected []interface{}
+		input OrderedStringMapType;
+		expected int
 	}
 
 	testData := []testRecord{
 		{
-			input: orderedMapType{0: "hi", 1: "hello", 2: "yoohoo", 3: "salut"},
-			expected: []interface{}{"hi", "hello", "yoohoo", "salut"},
+			input: OrderedStringMapType{0: "hi", 1: "hello", 2: "yoohoo", 3: "salut"},
+			expected: 4,
 		},
 		{
-			input: orderedMapType{0: 4, 1: 2, 2: "yoohoo", 3: "salut"},
-			expected: []interface{}{4, 2, "yoohoo", "salut"},
+			input: OrderedStringMapType{0: "4"},
+			expected: 1,
 		},
 		{
-			input: orderedMapType{0: 4, 2: "yoohoo", 3: "salut"},
-			expected: []interface{}{4, "yoohoo", "salut"},
+			input: OrderedStringMapType{0: "4", 2: "yoohoo", 3: "salut"},
+			expected: 3,
 		},
 	}
 
 	for _, tr := range testData {
-		got := tr.input.ToSlice()
-		if !utils.AreSliceEqual(got, tr.expected) {
+		got := tr.input.Len()
+		if got != tr.expected {
 			t.Fatalf("expected %v; got %v", tr.expected, got)
 		}
 	}
 }
 
-// Defragmentize should reorder the ordered dict basing
-// on the new indices passed to it
-func TestOrderedMap_Defragmentize(t *testing.T)  {
+func BenchmarkStringMap_Len(b *testing.B)  {
+	input := OrderedStringMapType{0: "hi", 1: "hello", 2: "yoohoo", 3: "salut"}
+
+	for i := 0; i < b.N; i++ {
+		input.Len()
+	}
+
+	// Results:
+	// ========
+	// 
+	// | Change 					| time				 | memory 				 | allocations			 | Choice  |
+	// |----------------------------|--------------------|-----------------------|-----------------------|---------|
+	// | None				  		| 0.4084 ns/op	     | 0 B/op	       		 | 0 allocs/op           |  x  	   |
+}
+
+// ToSlice converts an ordered map into a slice, ignoring gaps in indices automatically
+func TestOrderedStringMap_ToSlice(t *testing.T)  {
 	type testRecord struct {
-		_map orderedMapType;
-		newOrder []int;
-		expected []interface{}
+		input OrderedStringMapType;
+		expected []string
 	}
 
 	testData := []testRecord{
 		{
-			_map: orderedMapType{0: "hi", 1: "hello", 2: "yoohoo", 3: "salut"},
-			newOrder: []int{3, 1, 0},
-			expected: []interface{}{"salut", "hello", "hi"},
+			input: OrderedStringMapType{0: "hi", 1: "hello", 2: "yoohoo", 3: "salut"},
+			expected: []string{"hi", "hello", "yoohoo", "salut"},
 		},
 		{
-			_map: orderedMapType{0: 4, 1: 2, 2: "yoohoo", 3: "salut"},
+			input: OrderedStringMapType{0: "4", 1: "2", 2: "yoohoo", 3: "salut"},
+			expected: []string{"4", "2", "yoohoo", "salut"},
+		},
+		{
+			input: OrderedStringMapType{0: "4", 2: "yoohoo", 3: "salut"},
+			expected: []string{"4", "yoohoo", "salut"},
+		},
+	}
+
+	for _, tr := range testData {
+		got := tr.input.ToSlice()
+		if !utils.AreStringSliceEqual(got.([]string), tr.expected) {
+			t.Fatalf("expected %v; got %v", tr.expected, got)
+		}
+	}
+}
+
+func BenchmarkStringMap_ToSlice(b *testing.B)  {
+	input := OrderedStringMapType{0: "hi", 1: "hello", 2: "yoohoo", 3: "salut"}
+
+	for i := 0; i < b.N; i++ {
+		input.ToSlice()
+	}
+
+	// Results:
+	// ========
+	// 
+	// | Change 					| time				 | memory 				 | allocations			 | Choice  |
+	// |----------------------------|--------------------|-----------------------|-----------------------|---------|
+	// | None				  		| 546.5 ns/op	     | 176 B/op	       		 | 5 allocs/op           |  x  	   |
+}
+
+// Defragmentize should reorder the ordered dict basing
+// on the new indices passed to it
+func TestOrderedStringMap_Defragmentize(t *testing.T)  {
+	type testRecord struct {
+		_map OrderedStringMapType;
+		newOrder []int;
+		expected []string
+	}
+
+	testData := []testRecord{
+		{
+			_map: OrderedStringMapType{0: "hi", 1: "hello", 2: "yoohoo", 3: "salut"},
+			newOrder: []int{3, 1, 0},
+			expected: []string{"salut", "hello", "hi"},
+		},
+		{
+			_map: OrderedStringMapType{0: "4", 1: "2", 2: "yoohoo", 3: "salut"},
 			newOrder: []int{1, 0, 3, 2},
-			expected: []interface{}{2, 4, "salut", "yoohoo"},
+			expected: []string{"2", "4", "salut", "yoohoo"},
 		},
 	}
 
@@ -62,8 +122,472 @@ func TestOrderedMap_Defragmentize(t *testing.T)  {
 		tr._map.Defragmentize(tr.newOrder)
 		got := tr._map.ToSlice()
 
-		if !utils.AreSliceEqual(got, tr.expected) {
+		if !utils.AreStringSliceEqual(got.([]string), tr.expected) {
 			t.Fatalf("expected %v; got %v", tr.expected, got)
 		}
 	}
+}
+
+func BenchmarkStringMap_Defragmentize(b *testing.B)  {
+	input := OrderedStringMapType{0: "hi", 1: "hello", 3: "salut"}
+
+	for i := 0; i < b.N; i++ {
+		input.Defragmentize([]int{0, 3, 1})
+	}
+
+	// Results:
+	// ========
+	// 
+	// | Change 					| time				 | memory 				 | allocations			 | Choice  |
+	// |----------------------------|--------------------|-----------------------|-----------------------|---------|
+	// | None				  		| 292.3 ns/op	     | 256 B/op	       		 | 2 allocs/op           |  x  	   |
+}
+
+// Len returns the length of the map
+func TestOrderedIntMap_Len(t *testing.T)  {
+	type testRecord struct {
+		input OrderedIntMapType;
+		expected int
+	}
+
+	testData := []testRecord{
+		{
+			input: OrderedIntMapType{0: 4, 1: 67, 2: 90, 3: 8},
+			expected: 4,
+		},
+		{
+			input: OrderedIntMapType{0: 4},
+			expected: 1,
+		},
+		{
+			input: OrderedIntMapType{0: 4, 2: 67, 3: 76},
+			expected: 3,
+		},
+	}
+
+	for _, tr := range testData {
+		got := tr.input.Len()
+		if got != tr.expected {
+			t.Fatalf("expected %v; got %v", tr.expected, got)
+		}
+	}
+}
+
+func BenchmarkIntMap_Len(b *testing.B)  {
+	input := OrderedIntMapType{0: 4, 1: 67, 2: 90, 3: 8}
+
+	for i := 0; i < b.N; i++ {
+		input.Len()
+	}
+
+	// Results:
+	// ========
+	// 
+	// | Change 					| time				 | memory 				 | allocations			 | Choice  |
+	// |----------------------------|--------------------|-----------------------|-----------------------|---------|
+	// | None				  		| 0.8400 ns/op	     | 0 B/op	       		 | 0 allocs/op           |  x  	   |
+}
+
+// ToSlice converts an ordered map into a slice, ignoring gaps in indices automatically
+func TestOrderedIntMap_ToSlice(t *testing.T)  {
+	type testRecord struct {
+		input OrderedIntMapType;
+		expected []int
+	}
+
+	testData := []testRecord{
+		{
+			input: OrderedIntMapType{0: 4, 1: 67, 2: 90, 3: 8},
+			expected: []int{0: 4, 1: 67, 2: 90, 3: 8},
+		},
+		{
+			input: OrderedIntMapType{0: 4, 1: 67, 3: 8},
+			expected: []int{4, 67, 8},
+		},
+	}
+
+	for _, tr := range testData {
+		got := tr.input.ToSlice().([]int)
+		if len(got) != len(tr.expected) {
+			t.Fatalf("expected length: %d; got %d", len(got), len(tr.expected))
+		}
+
+		for i, v := range tr.expected {
+			gotValue := got[i]
+
+			if gotValue != v {
+				t.Fatalf("expected %v; got %v", v, gotValue)
+			}
+		}
+	}
+}
+
+func BenchmarkIntMap_ToSlice(b *testing.B)  {
+	input := OrderedIntMapType{0: 4, 1: 67, 2: 90, 3: 8}
+
+	for i := 0; i < b.N; i++ {
+		input.ToSlice()
+	}
+
+	// Results:
+	// ========
+	// 
+	// | Change 					| time				 | memory 				 | allocations			 | Choice  |
+	// |----------------------------|--------------------|-----------------------|-----------------------|---------|
+	// | None				  		| 427.4 ns/op	     | 144 B/op	       		 | 5 allocs/op           |  x  	   |
+}
+
+// Defragmentize should reorder the ordered dict basing
+// on the new indices passed to it
+func TestOrderedIntMap_Defragmentize(t *testing.T)  {
+	type testRecord struct {
+		_map OrderedIntMapType;
+		newOrder []int;
+		expected []int;
+	}
+
+	testData := []testRecord{
+		{
+			_map: OrderedIntMapType{0: 4, 1: 67, 2: 90, 3: 8},
+			newOrder: []int{3, 1, 0},
+			expected: []int{8, 67, 4},
+		},
+		{
+			_map: OrderedIntMapType{0: 4, 1: 67, 2: 90, 3: 8},
+			newOrder: []int{1, 0, 3, 2},
+			expected: []int{67, 4, 8, 90},
+		},
+	}
+
+	for _, tr := range testData {
+		tr._map.Defragmentize(tr.newOrder)
+		got := tr._map.ToSlice().([]int)
+
+		if len(got) != len(tr.expected) {
+			t.Fatalf("expected length: %d; got %d", len(got), len(tr.expected))
+		}
+
+		for i, v := range tr.expected {
+			gotValue := got[i]
+
+			if gotValue != v {
+				t.Fatalf("expected %v; got %v", v, gotValue)
+			}
+		}
+	}
+}
+
+func BenchmarkIntMap_Defragmentize(b *testing.B)  {
+	input := OrderedIntMapType{0: 4, 1: 67, 2: 90, 3: 8}
+
+	for i := 0; i < b.N; i++ {
+		input.Defragmentize([]int{0, 1, 3})
+	}
+
+	// Results:
+	// ========
+	// 
+	// | Change 					| time				 | memory 				 | allocations			 | Choice  |
+	// |----------------------------|--------------------|-----------------------|-----------------------|---------|
+	// | None				  		| 210.1 ns/op	     | 192 B/op	       		 | 2 allocs/op           |  x  	   |
+}
+
+// Len returns the length of the map
+func TestOrderedFloat64Map_Len(t *testing.T)  {
+	type testRecord struct {
+		input OrderedFloat64MapType;
+		expected int
+	}
+
+	testData := []testRecord{
+		{
+			input: OrderedFloat64MapType{0: 4, 1: 67, 2: 90, 3: 8},
+			expected: 4,
+		},
+		{
+			input: OrderedFloat64MapType{0: 4},
+			expected: 1,
+		},
+		{
+			input: OrderedFloat64MapType{0: 4, 2: 67, 3: 76},
+			expected: 3,
+		},
+	}
+
+	for _, tr := range testData {
+		got := tr.input.Len()
+		if got != tr.expected {
+			t.Fatalf("expected %v; got %v", tr.expected, got)
+		}
+	}
+}
+
+func BenchmarkFloat64Map_Len(b *testing.B)  {
+	input := OrderedFloat64MapType{0: 4, 1: 67, 2: 90, 3: 8}
+
+	for i := 0; i < b.N; i++ {
+		input.Len()
+	}
+
+	// Results:
+	// ========
+	// 
+	// | Change 					| time				 | memory 				 | allocations			 | Choice  |
+	// |----------------------------|--------------------|-----------------------|-----------------------|---------|
+	// | None				  		| 0.3927 ns/op	     | 0 B/op	       		 | 0 allocs/op           |  x  	   |
+}
+
+// ToSlice converts an ordered map into a slice, ignoring gaps in indices automatically
+func TestOrderedFloat64Map_ToSlice(t *testing.T)  {
+	type testRecord struct {
+		input OrderedFloat64MapType;
+		expected []float64
+	}
+
+	testData := []testRecord{
+		{
+			input: OrderedFloat64MapType{0: 4, 1: 67, 2: 90, 3: 8},
+			expected: []float64{0: 4, 1: 67, 2: 90, 3: 8},
+		},
+		{
+			input: OrderedFloat64MapType{0: 4, 1: 67, 3: 8},
+			expected: []float64{4, 67, 8},
+		},
+	}
+
+	for _, tr := range testData {
+		got := tr.input.ToSlice().([]float64)
+		
+		if len(got) != len(tr.expected) {
+			t.Fatalf("expected length: %d; got %d", len(got), len(tr.expected))
+		}
+
+		for i, v := range tr.expected {
+			gotValue := got[i]
+
+			if gotValue != v {
+				t.Fatalf("expected %v; got %v", v, gotValue)
+			}
+		}
+	}
+}
+
+func BenchmarkFloat64Map_ToSlice(b *testing.B)  {
+	input := OrderedFloat64MapType{0: 4, 1: 67, 2: 90, 3: 8}
+
+	for i := 0; i < b.N; i++ {
+		input.ToSlice()
+	}
+
+	// Results:
+	// ========
+	// 
+	// | Change 					| time				 | memory 				 | allocations			 | Choice  |
+	// |----------------------------|--------------------|-----------------------|-----------------------|---------|
+	// | None				  		| 631.9 ns/op	     | 144 B/op	       		 | 5 allocs/op           |  x  	   |
+}
+
+// Defragmentize should reorder the ordered dict basing
+// on the new indices passed to it
+func TestOrderedFloat64Map_Defragmentize(t *testing.T)  {
+	type testRecord struct {
+		_map OrderedFloat64MapType;
+		newOrder []int;
+		expected []float64
+	}
+
+	testData := []testRecord{
+		{
+			_map: OrderedFloat64MapType{0: 4, 1: 67, 2: 90, 3: 8},
+			newOrder: []int{3, 1, 0},
+			expected: []float64{8, 67, 4},
+		},
+		{
+			_map: OrderedFloat64MapType{0: 4, 1: 67, 2: 90, 3: 8},
+			newOrder: []int{1, 0, 3, 2},
+			expected: []float64{67, 4, 8, 90},
+		},
+	}
+
+	for _, tr := range testData {
+		tr._map.Defragmentize(tr.newOrder)
+		got := tr._map.ToSlice().([]float64)
+
+		if len(got) != len(tr.expected) {
+			t.Fatalf("expected length: %d; got %d", len(got), len(tr.expected))
+		}
+
+		for i, v := range tr.expected {
+			gotValue := got[i]
+
+			if gotValue != v {
+				t.Fatalf("expected %v; got %v", v, gotValue)
+			}
+		}
+	}
+}
+
+func BenchmarkFloat64Map_Defragmentize(b *testing.B)  {
+	input := OrderedFloat64MapType{0: 4, 1: 67, 2: 90, 3: 8}
+
+	for i := 0; i < b.N; i++ {
+		input.Defragmentize([]int{0, 3})
+	}
+
+	// Results:
+	// ========
+	// 
+	// | Change 					| time				 | memory 				 | allocations			 | Choice  |
+	// |----------------------------|--------------------|-----------------------|-----------------------|---------|
+	// | None				  		| 214.9 ns/op	     | 192 B/op	       		 | 2 allocs/op           |  x  	   |
+}
+
+// Len returns the length of the map
+func TestOrderedBoolMap_Len(t *testing.T)  {
+	type testRecord struct {
+		input OrderedBoolMapType;
+		expected int
+	}
+
+	testData := []testRecord{
+		{
+			input: OrderedBoolMapType{0: true, 1: true, 2: false, 3: true},
+			expected: 4,
+		},
+		{
+			input: OrderedBoolMapType{0: false},
+			expected: 1,
+		},
+		{
+			input: OrderedBoolMapType{0: true, 2: false, 3: true},
+			expected: 3,
+		},
+	}
+
+	for _, tr := range testData {
+		got := tr.input.Len()
+		if got != tr.expected {
+			t.Fatalf("expected %v; got %v", tr.expected, got)
+		}
+	}
+}
+
+func BenchmarkBoolMap_Len(b *testing.B)  {
+	input := OrderedBoolMapType{0: false, 1: true, 2: false, 3: true}
+
+	for i := 0; i < b.N; i++ {
+		input.Len()
+	}
+
+	// Results:
+	// ========
+	// 
+	// | Change 					| time				 | memory 				 | allocations			 | Choice  |
+	// |----------------------------|--------------------|-----------------------|-----------------------|---------|
+	// | None				  		| 0.8741 ns/op	     | 0 B/op	       		 | 0 allocs/op           |  x  	   |
+}
+
+// ToSlice converts an ordered map into a slice, ignoring gaps in indices automatically
+func TestOrderedBoolMap_ToSlice(t *testing.T)  {
+	type testRecord struct {
+		input OrderedBoolMapType;
+		expected []bool
+	}
+
+	testData := []testRecord{
+		{
+			input: OrderedBoolMapType{0: false, 1: true, 2: true, 3: false},
+			expected: []bool{false, true, true, false},
+		},
+		{
+			input: OrderedBoolMapType{0: false, 1: true, 3: false},
+			expected: []bool{false, true, false},
+		},
+	}
+
+	for _, tr := range testData {
+		got := tr.input.ToSlice().([]bool)
+
+		if len(got) != len(tr.expected) {
+			t.Fatalf("expected length: %d; got %d", len(got), len(tr.expected))
+		}
+
+		for i, v := range tr.expected {
+			gotValue := got[i]
+
+			if gotValue != v {
+				t.Fatalf("expected %v; got %v", v, gotValue)
+			}
+		}
+	}
+}
+
+func BenchmarkBoolMap_ToSlice(b *testing.B)  {
+	input := OrderedBoolMapType{0: false, 1: true, 2: false, 3: true}
+
+	for i := 0; i < b.N; i++ {
+		input.ToSlice()
+	}
+
+	// Results:
+	// ========
+	// 
+	// | Change 					| time				 | memory 				 | allocations			 | Choice  |
+	// |----------------------------|--------------------|-----------------------|-----------------------|---------|
+	// | None				  		| 418.6 ns/op	     | 116 B/op	       		 | 5 allocs/op           |  x  	   |
+}
+
+// Defragmentize should reorder the ordered dict basing
+// on the new indices passed to it
+func TestOrderedBoolMap_Defragmentize(t *testing.T)  {
+	type testRecord struct {
+		_map OrderedBoolMapType;
+		newOrder []int;
+		expected []bool
+	}
+
+	testData := []testRecord{
+		{
+			_map: OrderedBoolMapType{0: false, 1: true, 2: false, 3: false},
+			newOrder: []int{3, 1, 0},
+			expected: []bool{false, true, false},
+		},
+		{
+			_map: OrderedBoolMapType{0: false, 1: true, 2: false, 3: false},
+			newOrder: []int{1, 0, 3, 2},
+			expected: []bool{true, false, false, false},
+		},
+	}
+
+	for _, tr := range testData {
+		tr._map.Defragmentize(tr.newOrder)
+		got := tr._map.ToSlice().([]bool)
+
+		if len(got) != len(tr.expected) {
+			t.Fatalf("expected length: %d; got %d", len(got), len(tr.expected))
+		}
+
+		for i, v := range tr.expected {
+			gotValue := got[i]
+
+			if gotValue != v {
+				t.Fatalf("expected %v; got %v", v, gotValue)
+			}
+		}
+	}
+}
+
+func BenchmarkBoolMap_Defragmentize(b *testing.B)  {
+	input := OrderedBoolMapType{0: false, 1: true, 2: false, 3: true}
+
+	for i := 0; i < b.N; i++ {
+		input.Defragmentize([]int{1, 0})
+	}
+
+	// Results:
+	// ========
+	// 
+	// | Change 					| time				 | memory 				 | allocations			 | Choice  |
+	// |----------------------------|--------------------|-----------------------|-----------------------|---------|
+	// | None				  		| 162.3 ns/op	     | 144 B/op	       		 | 2 allocs/op           |  x  	   |
 }
