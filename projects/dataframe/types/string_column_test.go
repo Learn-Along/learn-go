078f46e79f1f16f126331b55aa23056b7662c738
Martin Ahindura <martinahindura@gmail.com>
2021-09-29 16:55:09 +0300

    Pass the test for LessOrEquals for StringColumn

--- projects/dataframe/types/string_column_test.go
+++ projects/dataframe/types/string_column_test.go
@@ -47,7 +47,7 @@ func TestStringColumn_GreaterThan(t *testing.T)  {
 		{
 			operand: "4", 
 			items: OrderedStringMapType{0: "23", 1: "6", 2: "2", 3: "69", 4: "0", 5: "67"},
-			expected: filterType{true, false, true, false, true, false},
+			expected: filterType{false, false, true, false, true, false},
 		},
 		{
 			operand: -2, 
@@ -84,7 +84,7 @@ func BenchmarkStringColumn_GreaterThan(b *testing.B)  {
 	col := StringColumn{name: "hi", items: items}
 
 	for i := 0; i < b.N; i++ {
-		col.GreaterThan(1000)
+		col.GreaterThan("1000")
 	}
 
 	// Results:
@@ -93,10 +93,7 @@ func BenchmarkStringColumn_GreaterThan(b *testing.B)  {
 	// 
 	// | Change 						| time				 | memory 				 | allocations			 | Choice  |
 	// |--------------------------------|--------------------|-----------------------|-----------------------|---------|
-	// | None				    		| 855,400,310 ns/op	 | 97,572,326 B/op	     | 775,572 allocs/op     | x  	   |
-	// | Add goroutine in for loop		| 4,449,787,656 ns/op| 363,255,202 B/op	     | 3,102,174 allocs/op   |		   |
-	// | With wrapper around goroutine	| 4,437,230,299 ns/op| 363251869 B/op	     | 3102156 allocs/op 	 |         |
-	// | With wait groups 				| 4,067,743,934 ns/op| 714,285,405 B/op	     | 8,164,777 allocs/op   |         |
+	// | Typed				    		| 1,640,126,487 ns/op| 90,068,110 B/op	 	 | 1307661 allocs/op   	 | x  	   |
 }
 
 // GreaterOrEquals should return a slice of booleans where true is for values greater or equal to the value,
@@ -117,17 +114,17 @@ func TestStringColumn_GreaterOrEquals(t *testing.T)  {
 		{
 			operand: -2, 
 			items: OrderedStringMapType{0: "23", 1: "6", 2: "-2", 3: "-69", 4: "-2", 5: "67"},
-			expected: filterType{false, false, true, true, true, false},
+			expected: filterType{false, false, false, false, false, false},
 		},
 		{
 			operand: StringColumn{name: "foo", items: OrderedStringMapType{0: "23", 1: "60", 2: "-2", 3: "69"}}, 
 			items: OrderedStringMapType{0: "23", 1: "6", 2: "-2", 3: "69", 4: "-2", 5: "67"},
-			expected: filterType{true, true, true, false, false, false},
+			expected: filterType{true, false, true, true, false, false},
 		},
 		{
-			operand: 4, 
+			operand: "4", 
 			items: OrderedStringMapType{0: "23", 1: "500", 2: "2", 3: "69", 4: "0", 5: "67"},
-			expected: filterType{true, true, false, true, false, true},
+			expected: filterType{false, true, false, true, false, true},
 		},
 	}
 
@@ -154,16 +151,16 @@ func BenchmarkStringColumn_GreaterOrEquals(b *testing.B)  {
 	col := StringColumn{name: "hi", items: items}
 
 	for i := 0; i < b.N; i++ {
-		col.GreaterOrEquals(1000)
+		col.GreaterOrEquals("1000")
 	}
 
 	// Results:
 	// ========
 	// benchtime=10s
 	// 
-	// | Change 						| time				 | memory 				 | allocations			 | Choice  |
-	// |--------------------------------|--------------------|-----------------------|-----------------------|---------|
-	// | None				    		| 871,616,373 ns/op	 | 97,562,900 B/op	     | 775,526 allocs/op     | x  	   |
+	// | Change 						| time				 	| memory 				 | allocations			 | Choice  |
+	// |--------------------------------|-----------------------|-----------------------|-----------------------|---------|
+	// | Typed							| 1,472,384,774 ns/op	| 90,057,602 B/op	 	 | 1,307,610 allocs/op	 | x	   |
 }
 
 // LessThan should return a slice of booleans where true is for values less than the value,
@@ -179,12 +176,12 @@ func TestStringColumn_LessThan(t *testing.T)  {
 		{
 			operand: "hi", 
 			items: OrderedStringMapType{0: "23", 1: "6", 2: "2", 3: "69", 4: "0", 5: "67"},
-			expected: filterType{false, false, false, false, false, false},
+			expected: filterType{true, true, true, true, true, true},
 		},
 		{
 			operand: -2, 
 			items: OrderedStringMapType{0: "23", 1: "6", 2: "-2", 3: "-69", 4: "-2", 5: "67"},
-			expected: filterType{true, true, false, false, false, false},
+			expected: filterType{false, false, false, false, false, false},
 		},
 		{
 			operand: StringColumn{name: "foo", items: OrderedStringMapType{0: "23", 1: "60", 2: "-2", 3: "69"}}, 
@@ -192,9 +189,9 @@ func TestStringColumn_LessThan(t *testing.T)  {
 			expected: filterType{false, true, false, false, false, false},
 		},
 		{
-			operand: 4, 
+			operand: "4", 
 			items: OrderedStringMapType{0: "23", 1: "500", 2: "2", 3: "69", 4: "0", 5: "67"},
-			expected: filterType{true, true, false, true, false, true},
+			expected: filterType{true, false, true, false, true, false},
 		},
 	}
 
@@ -221,7 +218,7 @@ func BenchmarkStringColumn_LessThan(b *testing.B)  {
 	col := StringColumn{name: "hi", items: items}
 
 	for i := 0; i < b.N; i++ {
-		col.LessThan(1000)
+		col.LessThan("1000")
 	}
 
 	// Results:
@@ -230,7 +227,7 @@ func BenchmarkStringColumn_LessThan(b *testing.B)  {
 	// 
 	// | Change 						| time				 	| memory 				 | allocations			 | Choice  |
 	// |--------------------------------|-----------------------|------------------------|-----------------------|---------|
-	// | None				    		| 1,142,363,432 ns/op	|	97,571,606 B/op	     | 775,569 allocs/op     | x  	   |
+	// | None				    		| 1,859,208,357 ns/op	| 103569454 B/op	 	 | 1525559 allocs/op     | x  	   |
 }
 
 // LessOrEquals should return a slice of booleans where true is for values less or equal to the value,
@@ -246,22 +243,22 @@ func TestStringColumn_LessOrEquals(t *testing.T)  {
 		{
 			operand: "hi", 
 			items: OrderedStringMapType{0: "23", 1: "6", 2: "2", 3: "69", 4: "0", 5: "67"},
-			expected: filterType{false, false, false, false, false, false},
+			expected: filterType{true, true, true, true, true, true},
 		},
 		{
 			operand: -2, 
 			items: OrderedStringMapType{0: "23", 1: "6", 2: "-2", 3: "-69", 4: "-2", 5: "67"},
-			expected: filterType{true, true, true, false, true, false},
+			expected: filterType{false, false, false, false, false, false},
 		},
 		{
 			operand: StringColumn{name: "foo", items: OrderedStringMapType{0: "23", 1: "60", 2: "-2", 3: "69"}}, 
-			items: OrderedStringMapType{0: "23", 1: "6", 2: "-2", 3: "69", 4: "-2", 5: "67"},
-			expected: filterType{true, false, true, true, false, false},
+			items: OrderedStringMapType{0: "23", 1: "6", 2: "-2", 3: "690", 4: "-2", 5: "67"},
+			expected: filterType{true, true, true, false, false, false},
 		},
 		{
-			operand: 4, 
+			operand: "4", 
 			items: OrderedStringMapType{0: "23", 1: "500", 2: "2", 3: "69", 4: "0", 5: "67"},
-			expected: filterType{true, true, false, true, false, true},
+			expected: filterType{true, false, true, false, true, false},
 		},
 	}
 
@@ -288,7 +285,7 @@ func BenchmarkStringColumn_LessOrEquals(b *testing.B)  {
 	col := StringColumn{name: "hi", items: items}
 
 	for i := 0; i < b.N; i++ {
-		col.LessOrEquals(1000)
+		col.LessOrEquals("1000")
 	}
 
 	// Results:
@@ -297,7 +294,7 @@ func BenchmarkStringColumn_LessOrEquals(b *testing.B)  {
 	// 
 	// | Change 						| time				 	| memory 				| allocations			| Choice  |
 	// |--------------------------------|-----------------------|-----------------------|-----------------------|---------|
-	// | None				    		| 5,437,853,397 ns/op	| 540,413,572 B/op	    | 4653405 allocs/op     | x  	  |
+	// | None				    		| 2,056,327,446 ns/op	| 103,567,231 B/op	 	| 1,525,548 allocs/op   | x  	  |
 }
 
 // Equals should return a slice of booleans where true is for values equal to the value,
